1240befc8262bed1748e968655fb2f4d02153c90
Nagu <154983315+H277353@users.noreply.github.com>
2025-11-11 21:10:00 +0530

    login signup with bcryptjs , yet to add jwt

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -1,11 +1,35 @@
+import { PrismaClient } from "@/lib/generated/prisma/client"
+import bcrypt from "bcryptjs"
+import { NextResponse } from "next/server"
+import z from "zod"
 
 
+const loginSchema = z.object({
+    email: z.string().email(),
+    password: z.string()
+})
 
 
 export async function POST(request : Request){
-    const { email, password } = await request.json()
-    
-    return new Response(JSON.stringify({email, password}), {
-        status:200
-    })
+    try {
+        const { email, password } = await request.json()
+        const validateInput = loginSchema.parse({ email, password })
+       
+        const prisma = new PrismaClient()
+        const exists = await prisma.user.findUnique({
+            where: {
+              email:validateInput.email  
+            }
+        })
+        if(!exists) return NextResponse.json({message:"Invalid"})
+       
+        const matchPass = bcrypt.compareSync(validateInput.password, exists?.password)
+        if (matchPass) return NextResponse.json({ message: "Login successfull!" }, { status: 200 })
+        else return NextResponse.json({ message: "Inavlid Credentails" }, { status: 400})
+    } catch (error) {
+        if (error instanceof z.ZodError) {
+            return NextResponse.json({ errors: error }, { status: 400})
+        }
+        return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
+    }
 }
--- app/api/auth/signup/route.ts
+++ app/api/auth/signup/route.ts
@@ -1,18 +1,37 @@
 import { PrismaClient } from "@/lib/generated/prisma/client"
+import {z} from "zod"
+import bcrypt from "bcryptjs";
+import { NextResponse } from "next/server";
 
+const signUpSchema = z.object({
+    username: z.string().min(3),
+    email: z.string().email(),
+    password: z.string()
+})
 
 export async function POST(request : Request){
-    const { email, password, username } = await request.json()
+    try {
+        const { email, password, username } = await request.json()
     const prisma = new PrismaClient()
+    const validateInput = signUpSchema.parse({ username, email, password })
+    
+    const salt = bcrypt.genSaltSync()
+    const hashedPass = bcrypt.hashSync(validateInput.password,salt)
+
     const newUser = await prisma.user.create({
                             data: {
-                                email,
-                                password,
-                                username
+                                email:validateInput.email,
+                                password:hashedPass,
+                                username:validateInput.username
                             }
                             })
 
-    return new Response(JSON.stringify({email, password}), {
-        status:200
-    })
+        const message = `User ${newUser.username} created successfully!!`
+        return NextResponse.json({ message }, { status: 201 })
+    } catch (error) {
+        if (error instanceof z.ZodError) {
+            return NextResponse.json({errors: error}, {status:400})
+        }
+        return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
+    }
 }
